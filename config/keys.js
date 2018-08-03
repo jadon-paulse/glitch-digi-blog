6c07c2320b6229dc088c4d23d22d5ebb91ec0123
Glitch (kaput-weed) <none>
2018-08-03 09:57:53 +0000

    ⛵️💏 Updated with Glitch

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:"195358092336-b6nrusgb32sj3fbluliuplcjstf14hlg.apps.googleusercontent.com",
+    clientSecret:"E1mzQq_EAyaBkLtWjH4g5Qa3"
+  }
+};
